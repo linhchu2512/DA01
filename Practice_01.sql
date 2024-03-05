9e622aeb032cc24a8256bbe4c2b09a692c06825a
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-05 22:59:21 +0700

    Update Practice_01.sql

--- Practice_01.sql
+++ Practice_01.sql
@@ -15,3 +15,22 @@ select name from employee
 order by name;
 
 --Ex8:
+select name from employee
+where salary > 2000 and months <10;
+
+--Ex9:
+select product_id from products
+where low_fats = 'Y' and recyclable = 'Y';
+
+--Ex10:
+select name from customer
+where referee_id is null or referee_id = 2;
+
+--Ex11:
+select name, population, area from world
+where population >= 25000000 and area >= 3000000;
+
+--Ex12:
+
+
+
