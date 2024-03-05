dd2c1e8e179f11ddc05577b74e437ed9d927ca40
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-05 22:11:12 +0700

    Update Practice_01.sql

--- Practice_01.sql
+++ Practice_01.sql
@@ -6,3 +6,12 @@ where countrycode='USA' and population > 120000;
 select * from city
 where countrycode = 'JPN';
 
+--Ex3:
+select city, state from station;
+
+--Ex4:
+--Ex7:
+select name from employee
+order by name;
+
+--Ex8:
