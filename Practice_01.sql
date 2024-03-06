40c3c96e55540930c8140b6531e6265dde38dfc8
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-06 10:48:53 +0700

    Update Practice_01.sql

--- Practice_01.sql
+++ Practice_01.sql
@@ -10,6 +10,17 @@ where countrycode = 'JPN';
 select city, state from station;
 
 --Ex4:
+select distinct city from station
+where city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%';
+
+--Ex5:
+select distinct city from station
+where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+
+--Ex6:
+select distinct city from station
+where not (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%');
+
 --Ex7:
 select name from employee
 order by name;
@@ -24,13 +35,19 @@ where low_fats = 'Y' and recyclable = 'Y';
 
 --Ex10:
 select name from customer
-where referee_id is null or referee_id = 2;
+where referee_id is null or referee_id <> 2;
 
 --Ex11:
 select name, population, area from world
-where population >= 25000000 and area >= 3000000;
+where population >= 25000000 or area >= 3000000;
 
 --Ex12:
+select distinct author_id as 'id' from views
+where author_id = viewer_id
+order by author_id;
 
+--Ex13:
+select part, assembly_step from parts_assembly
+where finish_date is null;
 
-
+--Ex14:
