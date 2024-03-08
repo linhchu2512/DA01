f6da0c4a65663b807a50d904f85b541aec8f5aa9
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-08 22:18:36 +0700

    Update Practice_02.sql

--- Practice_02.sql
+++ Practice_02.sql
@@ -19,3 +19,41 @@ having count(skill = 3)
 order by candidate_id;
 
 --Ex6:
+
+--Ex7:
+SELECT card_name, max (issued_amount)-min (issued_amount) as Difference
+FROM monthly_cards_issued
+group by card_name
+order by Difference desc;
+
+--Ex8:
+SELECT manufacturer,
+count (drug) as drug_count,
+abs(sum (cogs-total_sales)) as total_loss
+FROM pharmacy_sales
+where total_sales - cogs < 0
+group by manufacturer
+order by total_loss desc;
+
+--Ex9:
+select * from cinema
+where id%2=1 and description <> 'boring'
+order by rating desc;
+
+--Ex10:
+select teacher_id,
+count (distinct subject_id) as cnt
+from teacher
+group by teacher_id;
+
+--Ex11:
+select user_id,
+count (follower_id) as followers_count
+from followers
+group by user_id
+order by user_id;
+
+--Ex12:
+select class from courses
+group by class
+having count (student) >= 5;
