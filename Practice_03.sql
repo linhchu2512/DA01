2fbe9aae02f6bbd914b98d51df08ee2b99aa9d4c
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-10 20:06:46 +0700

    Update Practice_03.sql

--- Practice_03.sql
+++ Practice_03.sql
@@ -37,3 +37,13 @@ limit 2;
 --Ex6:
 select tweet_id from tweets
 where length(content)>15;
+
+--Ex7:
+select distinct activity_date,
+count distinct (user_id) as active_users
+from activity
+where activity_date between dateadd(day,-30,2019-07-27) and '2019-07-27'
+group by activity_date;
+
+--Ex8:
+
