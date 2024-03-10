a244171233f5bcda99d832b0b981d7b973fc6571
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-10 12:07:10 +0700

    Update Practice_03.sql

--- Practice_03.sql
+++ Practice_03.sql
@@ -10,3 +10,10 @@ from users
 order by user_id;
 
 --Ex3:
+SELECT manufacturer,
+concat('$',round(sum(total_sales)/1000000,0),' ','million') as sale
+FROM pharmacy_sales
+group by manufacturer
+order by sum(total_sales) desc, manufacturer;
+
+--Ex4:
