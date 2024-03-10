760aa5cbf5330d1eddd189c5cadf6092e2dc6ed4
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-10 09:17:35 +0700

    Update Practice_03.sql

--- Practice_03.sql
+++ Practice_03.sql
@@ -2,3 +2,11 @@
 select name from students
 where marks > 75
 order by right (name,3), id;
+
+--Ex2:
+select user_id, 
+concat(upper (left (name,1), lower (right (name, length (name)-1))) as name
+from users
+order by user_id;
+
+--Ex3:
