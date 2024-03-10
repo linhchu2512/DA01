e7b810ef8e4a0efe8c67916931c99c0678934624
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-10 20:47:43 +0700

    Update Practice_03.sql

--- Practice_03.sql
+++ Practice_03.sql
@@ -42,7 +42,7 @@ where length(content)>15;
 select distinct activity_date,
 count distinct (user_id) as active_users
 from activity
-where activity_date between dateadd(day,-30,2019-07-27) and '2019-07-27'
+where activity_date between dateadd('2019-07-27',-30,day) and '2019-07-27'
 group by activity_date;
 
 --Ex8:
