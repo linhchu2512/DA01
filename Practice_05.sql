a11add61c5f80654dcb1ceef523cdec9e5ef7c68
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-16 20:28:37 +0700

    Update Practice_05.sql

--- Practice_05.sql
+++ Practice_05.sql
@@ -2,3 +2,9 @@
 select a.continent, round(avg(b.population),0) from country as a 
 inner join city as b on b.countrycode = a.code
 group by a.continent;
+
+--Ex2:
+SELECT round(cast(count(t.signup_action)/count(distinct e.email_id)) as decimal),2) as activation_rate
+FROM emails as e left join texts as t 
+on e.email_id = t.email_id
+where t.signup_action = 'Confirm';
