0562d04695f8cbec76f39fd5aa83c9f9d6f1984c
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-20 20:57:28 +0700

    Update Mid-course test.sql

--- Mid-course test.sql
+++ Mid-course test.sql
@@ -24,3 +24,22 @@ group by category.name
 order by count(film.title) desc;
 
 --Ex5:
+select a.first_name ||' '|| a.last_name as name_actor, count (fa.film_id)
+from actor as a join film_actor as fa on a.actor_id = fa.actor_id
+group by name_actor
+order by count (fa.film_id) desc;
+
+--Ex6:
+select ad.address_id, ad.address, cus.customer_id
+from address as ad left join customer as cus
+on ad.address_id = cus.address_id
+where cus.customer_id is null
+order by address_id;
+
+--Ex7:
+select ci.city, sum(p.amount) as amount
+from city as ci join address as a on ci.city_id = a.city_id
+join customer as cus on cus.address_id = a.address_id
+join payment as p on p.customer_id = cus.customer_id
+group by ci.city
+order by amount desc;
