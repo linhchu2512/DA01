3abd0304d8c66df778cd9f58e6ff6f4a85d977e7
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-24 20:55:21 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -8,3 +8,16 @@ from job_count
 where job_count > 1;
 
 --Ex2:
+
+--Ex3:
+with call_records as (
+SELECT policy_holder_id, 
+count (case_id) as call_count
+FROM callers
+group by policy_holder_id
+having call_count >= 3
+)
+select count (policy_holder_id) as member_count
+from call_records;
+
+--Ex4:
