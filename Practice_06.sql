b45f2cb17a7f84d4f53b2994b1261d89b973fcad
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-25 12:12:11 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -25,3 +25,18 @@ SELECT pages.page_id FROM pages
 left join page_likes on pages.page_id = page_likes.page_id
 where page_likes.liked_date is null
 order by pages.page_id asc;
+
+--Ex5:
+SELECT
+extract (month from cur_mth.envent_date) as month,
+count (distinct cur_mth.user_id) as monthly_active_users
+from user_actions as cur_mth
+where exists (
+select last_mth.user_id
+from user_actions as last_mth
+where last_mth.user_id = cur_mth.user_id
+and extract (month from last_mth.event_date) =
+extract (month from cur_mth.event_date - interval '1 month'))
+and extract (month from cur_mth.event_date) = 7
+and extract (year from cur_mth.event_date) = 2022
+group by extract (month from cur_mth.event_date);
