9ffc34fbbcd6dcaaef7feae1d7977cc0bb656df3
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-03-29 12:01:18 +0700

    Update Practice_07.sql

--- Practice_07.sql
+++ Practice_07.sql
@@ -1,1 +1,16 @@
+--Ex1:
+With twt_transaction AS
+(
+SELECT 
+extract(year from transaction_date) as year,
+product_id,
+sum (spend) as curr_year_spend
+FROM user_transactions
+group by product_id, year
+)
+SELECT year, product_id, curr_year_spend,
+lag(curr_year_spend) over(partition by product_id order by year) as prev_year_spend,
+round(100*(curr_year_spend-lag(curr_year_spend) over(partition by product_id order by year))/lag(curr_year_spend) over(partition by product_id order by year),2) as yoy_rate
+from twt_transaction
 
+--Ex2:
