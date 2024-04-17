168efbc6665ba07efc943f2343742a2d9f2fd474
linhchu2512 <162334830+linhchu2512@users.noreply.github.com>
2024-04-17 15:04:04 +0700

    Create Project_02.sql

---
+++ Project_02.sql
@@ -0,0 +1,7 @@
+/*Ecommerce Dataset: Exploratory Data Analysis (EDA) and Cohort Analysis in SQL
+--Tổng quan về dữ liệu
+TheLook là một trang web thương mại điện tử về quần áo. Tập dữ liệu chứa thông tin về customers, products, orders, logistics, web events , digital marketing campaigns.
+--Các bảng:
+Bảng Orders : ghi lại tất cả các đơn hàng mà khách hàng đã đặt
+Bảng Order_items : ghi lại danh sách các mặt hàng đã mua trong mỗi order ID..
+Bảng Products : ghi lại chi tiết các sản phẩm được bán trên The Look, bao gồm price, brand, & product categories
